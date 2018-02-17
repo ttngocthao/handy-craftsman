e736037eb63977f8ec43393ac569bda9f3ab5c0b
ttngocthao <36475218+ttngocthao@users.noreply.github.com>
2018-02-17 16:37:46 +0000

    handy-craftsman

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#contact').on('click',function(){
+        $('.contact').fadeToggle('slow');
+    });
+    /*$('img').mouseenter(function(){
+        $(this).animate({
+            height: '+=20px'
+        });
+    });
+    $('img').mouseleave(function(){
+        $(this).animate({
+            height: '-=20px'
+        });
+    });*/
+    
+    
+});
